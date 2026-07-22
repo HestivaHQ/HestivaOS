bcda5a1684de2213581ce501d2333537e4f9988a
maintainance-marshall <info@maintenancemarshall.co.za>
2026-07-22 19:35:02 +0200

    Register customer module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CustomersModule } from './customers/customers.module';
 import { HealthController } from './health.controller';
 import { PrismaService } from './prisma.service';
 
 @Module({
+  imports: [CustomersModule],
   controllers: [HealthController],
   providers: [PrismaService],
 })
