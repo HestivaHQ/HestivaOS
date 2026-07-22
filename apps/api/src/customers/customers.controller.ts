a4f5bb1e09ac97606bbae7c3a1e1dc237372a6cc
maintainance-marshall <info@maintenancemarshall.co.za>
2026-07-22 19:34:36 +0200

    Add customer controller

---
+++ apps/api/src/customers/customers.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CustomerStatus } from '@prisma/client';
+import {
+  CreateCustomerInput,
+  CustomersService,
+  UpdateCustomerInput,
+} from './customers.service';
+
+@Controller('customers')
+export class CustomersController {
+  constructor(private readonly customers: CustomersService) {}
+
+  @Post()
+  create(@Body() input: CreateCustomerInput) {
+    return this.customers.create(input);
+  }
+
+  @Get()
+  findAll(
+    @Query('page', new ParseIntPipe({ optional: true })) page?: number,
+    @Query('pageSize', new ParseIntPipe({ optional: true })) pageSize?: number,
+    @Query('search') search?: string,
+    @Query('status') status?: CustomerStatus,
+  ) {
+    return this.customers.findAll(page, pageSize, search, status);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.customers.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() input: UpdateCustomerInput,
+  ) {
+    return this.customers.update(id, input);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.customers.remove(id);
+  }
+}
