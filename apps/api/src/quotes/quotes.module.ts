e22f5662af6017038e89aee52b92512969bf2c6c
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-13 18:56:25 +0200

    fix(quotes): restore module provider

--- apps/api/src/quotes/quotes.module.ts
+++ apps/api/src/quotes/quotes.module.ts
@@ -1,1 +1,20 @@
-noop
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { QUOTE_OPERATIONAL_COST_PROVIDER } from './quote-operational-cost-source';
+import { WebsiteQuoteIngestionController } from './website-quote-ingestion.controller';
+import { WebsiteQuoteIngestionService } from './website-quote-ingestion.service';
+
+@Module({
+  controllers: [WebsiteQuoteIngestionController],
+  providers: [
+    PrismaService,
+    WebsiteQuoteIngestionService,
+    {
+      provide: QUOTE_OPERATIONAL_COST_PROVIDER,
+      useValue: {
+        resolve: async () => null,
+      },
+    },
+  ],
+})
+export class QuotesModule {}
