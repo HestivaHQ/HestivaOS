d8be0e34be96a0028e7caa93fb1646b588afdc0d
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-18 22:53:07 +0200

    technician: register interrupted visit controller

--- apps/api/src/technician-jobs/technician-jobs.module.ts
+++ apps/api/src/technician-jobs/technician-jobs.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { WorkOrdersModule } from '../work-orders/work-orders.module';
+import { TechnicianInterruptionsController } from './technician-interruptions.controller';
 import { TechnicianJobsController } from './technician-jobs.controller';
 import { TechnicianJobsService } from './technician-jobs.service';
 
-@Module({ controllers: [TechnicianJobsController], providers: [TechnicianJobsService, PrismaService] })
+@Module({ imports: [WorkOrdersModule], controllers: [TechnicianJobsController, TechnicianInterruptionsController], providers: [TechnicianJobsService, PrismaService] })
 export class TechnicianJobsModule {}
