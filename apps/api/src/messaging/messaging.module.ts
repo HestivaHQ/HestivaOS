57e77fcf8251ce23bca5e34b7f8ff35879596a1d
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-20 19:11:44 +0200

    Wire inbound WhatsApp media storage

--- apps/api/src/messaging/messaging.module.ts
+++ apps/api/src/messaging/messaging.module.ts
@@ -3,12 +3,13 @@ import { PrismaService } from '../prisma.service';
 import { MessagingAdapterRegistry } from './messaging-adapter-registry';
 import { MessagingService } from './messaging.service';
 import { WhatsAppCloudApiAdapter } from './whatsapp-cloud-api.adapter';
+import { WhatsAppInboundMediaService } from './whatsapp-inbound-media.service';
 import { WhatsAppWebhookController } from './whatsapp-webhook.controller';
 
 @Global()
 @Module({
   controllers: [WhatsAppWebhookController],
-  providers: [PrismaService, MessagingAdapterRegistry, MessagingService, WhatsAppCloudApiAdapter],
+  providers: [PrismaService, MessagingAdapterRegistry, MessagingService, WhatsAppCloudApiAdapter, WhatsAppInboundMediaService],
   exports: [MessagingAdapterRegistry, MessagingService],
 })
 export class MessagingModule {}
