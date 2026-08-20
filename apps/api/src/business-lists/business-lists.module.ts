f23d4fed507fc4a4d4e8968f6eb7ce20b967adc1
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-21 01:41:17 +0200

    Reuse global Prisma service in business lists

--- apps/api/src/business-lists/business-lists.module.ts
+++ apps/api/src/business-lists/business-lists.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "../prisma.service";
 import { BusinessListsController } from "./business-lists.controller";
 import { BusinessListsService } from "./business-lists.service";
 @Module({
   controllers: [BusinessListsController],
-  providers: [BusinessListsService, PrismaService],
+  providers: [BusinessListsService],
 })
 export class BusinessListsModule {}
