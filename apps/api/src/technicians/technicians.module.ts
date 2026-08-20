e5086b36fe8ed0cbd4059891285fb44863e28176
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-21 01:41:08 +0200

    Reuse global Prisma service in technicians

--- apps/api/src/technicians/technicians.module.ts
+++ apps/api/src/technicians/technicians.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { TechniciansController } from './technicians.controller';
 import { TechniciansService } from './technicians.service';
 
 @Module({
   controllers: [TechniciansController],
-  providers: [TechniciansService, PrismaService],
+  providers: [TechniciansService],
   exports: [TechniciansService],
 })
 export class TechniciansModule {}
