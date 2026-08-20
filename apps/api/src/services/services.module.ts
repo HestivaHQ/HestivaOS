3a320913c5e66adc441b89a6137406846ca4a568
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-21 01:39:55 +0200

    Reuse global Prisma service in services

--- apps/api/src/services/services.module.ts
+++ apps/api/src/services/services.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
 
 @Module({
   controllers: [ServicesController],
-  providers: [ServicesService, PrismaService],
+  providers: [ServicesService],
   exports: [ServicesService],
 })
 export class ServicesModule {}
