cf18f9939bdd7725c1d0b4f1068d4f7f5e236622
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-21 01:40:13 +0200

    Reuse global Prisma service in enquiries

--- apps/api/src/enquiries/enquiries.module.ts
+++ apps/api/src/enquiries/enquiries.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { WebsiteEnquiryIngestionController } from './website-enquiry-ingestion.controller';
 import { WebsiteEnquiryIngestionService } from './website-enquiry-ingestion.service';
 
 @Module({
   controllers: [WebsiteEnquiryIngestionController],
-  providers: [PrismaService, WebsiteEnquiryIngestionService],
+  providers: [WebsiteEnquiryIngestionService],
 })
 export class EnquiriesModule {}
