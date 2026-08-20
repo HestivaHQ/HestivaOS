fa6c6b7731bd82f610cb1770f77ec0eef54bbeaa
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-20 15:13:41 +0200

    feat: wire work order correspondence events

--- apps/api/src/correspondence/correspondence.module.ts
+++ apps/api/src/correspondence/correspondence.module.ts
@@ -3,10 +3,11 @@ import { PrismaService } from '../prisma.service';
 import { CorrespondenceRecordsController } from './correspondence-records.controller';
 import { CorrespondenceController } from './correspondence.controller';
 import { CorrespondenceService } from './correspondence.service';
+import { CorrespondenceWorkOrderEventsService } from './correspondence-work-order-events.service';
 
 @Module({
   controllers: [CorrespondenceController, CorrespondenceRecordsController],
-  providers: [CorrespondenceService, PrismaService],
-  exports: [CorrespondenceService],
+  providers: [CorrespondenceService, CorrespondenceWorkOrderEventsService, PrismaService],
+  exports: [CorrespondenceService, CorrespondenceWorkOrderEventsService],
 })
 export class CorrespondenceModule {}
