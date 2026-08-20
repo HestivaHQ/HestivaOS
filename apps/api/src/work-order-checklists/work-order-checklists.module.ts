015715d67bd6b95ab531f9c382bcf8a424d2423a
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-21 01:42:23 +0200

    Reuse global Prisma service in work order checklists

--- apps/api/src/work-order-checklists/work-order-checklists.module.ts
+++ apps/api/src/work-order-checklists/work-order-checklists.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { WorkOrderChecklistsController } from './work-order-checklists.controller';
 import { WorkOrderChecklistsService } from './work-order-checklists.service';
 
 @Module({
   controllers: [WorkOrderChecklistsController],
-  providers: [WorkOrderChecklistsService, PrismaService],
+  providers: [WorkOrderChecklistsService],
 })
 export class WorkOrderChecklistsModule {}
