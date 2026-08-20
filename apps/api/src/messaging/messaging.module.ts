12475d623c8c073d591051db176e6f36e39810ef
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-20 19:43:18 +0200

    Wire Messenger inbound provider edge

--- apps/api/src/messaging/messaging.module.ts
+++ apps/api/src/messaging/messaging.module.ts
@@ -2,14 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { MessagingAdapterRegistry } from './messaging-adapter-registry';
 import { MessagingService } from './messaging.service';
+import { MessengerPlatformAdapter } from './messenger-platform.adapter';
+import { MessengerWebhookController } from './messenger-webhook.controller';
 import { WhatsAppCloudApiAdapter } from './whatsapp-cloud-api.adapter';
 import { WhatsAppInboundMediaService } from './whatsapp-inbound-media.service';
 import { WhatsAppWebhookController } from './whatsapp-webhook.controller';
 
 @Global()
 @Module({
-  controllers: [WhatsAppWebhookController],
-  providers: [PrismaService, MessagingAdapterRegistry, MessagingService, WhatsAppCloudApiAdapter, WhatsAppInboundMediaService],
+  controllers: [WhatsAppWebhookController, MessengerWebhookController],
+  providers: [PrismaService, MessagingAdapterRegistry, MessagingService, WhatsAppCloudApiAdapter, WhatsAppInboundMediaService, MessengerPlatformAdapter],
   exports: [MessagingAdapterRegistry, MessagingService],
 })
 export class MessagingModule {}
