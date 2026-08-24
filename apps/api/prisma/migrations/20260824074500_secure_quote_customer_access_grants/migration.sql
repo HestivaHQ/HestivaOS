892bb6d19549c944cf02d95773600a7d549a5b98
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-24 09:58:37 +0200

    Make Quote grant supersession atomic

--- apps/api/prisma/migrations/20260824074500_secure_quote_customer_access_grants/migration.sql
+++ apps/api/prisma/migrations/20260824074500_secure_quote_customer_access_grants/migration.sql
@@ -22,7 +22,7 @@ CREATE TABLE "quote_customer_access_grants" (
   CONSTRAINT "quote_customer_access_grants_revoker_fkey"
     FOREIGN KEY ("revoked_by_user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT "quote_customer_access_grants_superseded_by_fkey"
-    FOREIGN KEY ("superseded_by_grant_id") REFERENCES "quote_customer_access_grants"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY ("superseded_by_grant_id") REFERENCES "quote_customer_access_grants"("id") ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED,
   CONSTRAINT "quote_customer_access_grants_terminal_shape_check" CHECK (
     NOT ("revoked_at" IS NOT NULL AND "superseded_at" IS NOT NULL)
     AND (("revoked_at" IS NULL) = ("revoked_by_user_id" IS NULL))
