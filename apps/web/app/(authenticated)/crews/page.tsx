8dcb3cd359116fd918fe8de5b85d832b9ff33c6b
HestivaHQ <smangalisonkosi96@gmail.com>
2026-08-29 14:34:15 +0200

    Server-load crew workspace

--- apps/web/app/(authenticated)/crews/page.tsx
+++ apps/web/app/(authenticated)/crews/page.tsx
@@ -2,6 +2,10 @@ import { createAuthenticatedApi } from '../../../lib/api-server';
 import { CrewsManager } from '../../crews/crews-manager';
 
 export default async function CrewsPage() {
-  const appUser = await (await createAuthenticatedApi()).currentUser();
-  return <><div className="fieldTeamWorkspace"><CrewsManager /></div></>;
+  const authenticatedApi = await createAuthenticatedApi();
+  const [crews, technicians] = await Promise.all([
+    authenticatedApi.crews('?page=1&pageSize=100'),
+    authenticatedApi.technicians('?page=1&pageSize=100'),
+  ]);
+  return <><div className="fieldTeamWorkspace"><CrewsManager initialItems={crews.items} initialTechnicians={technicians.items} /></div></>;
 }
